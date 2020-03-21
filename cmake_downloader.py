144ef2195b1ccae19b8a8d8f18ca72ac7ea7f480
Niels Lohmann <mail@nlohmann.me>
2020-03-21 10:23:05 +0100

    :sparkles: add parameters for download script

--- cmake_downloader.py
+++ cmake_downloader.py
@@ -1,10 +1,11 @@
 import argparse
 import os.path
 import re
-from typing import List
+from typing import List, Dict
 
-import progressbar
 import requests
+from tqdm import tqdm
+from packaging.version import parse as version_parse
 
 
 def get_folders() -> List[str]:
@@ -22,10 +23,9 @@ def get_tarball_urls_version(base_version: str) -> List[str]:
 def get_tarball_urls() -> List[str]:
     folders = get_folders()
     result = []  # type: List[str]
-    progress = progressbar.ProgressBar()
 
     print('Retrieving URLs...')
-    for folder in progress(folders):
+    for folder in tqdm(folders):
         urls = get_tarball_urls_version(folder)
         result += urls
 
@@ -41,30 +41,56 @@ def download_file(url: str):
 
     file_size = int(response.headers['Content-Length'])
 
-    widgets = [progressbar.Bar(), progressbar.DataSize(), progressbar.FileTransferSpeed(), ' (', progressbar.ETA(), ')']
-    progress = progressbar.ProgressBar(max_value=file_size, widgets=widgets)
-    downloaded_bytes = 0
-
     if not os.path.exists(file_name) or os.path.getsize(file_name) != file_size:
+        progress = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
         with open(file_name, 'wb+') as f:
             for data in response:
-                downloaded_bytes += len(data)
-                progress.update(downloaded_bytes)
+                progress.update(len(data))
                 f.write(data)
-        progress.finish()
+        progress.close()
+
+
+def create_version_dict() -> Dict[str, str]:
+    tarball_urls = get_tarball_urls()
+    result = dict()
+
+    for tarball_url in tarball_urls:
+        version = re.findall(r'cmake-(([0-9.]+)(-rc[0-9]+)?)', tarball_url)[0][0]
+
+        if 'Darwin64' in tarball_url or 'Darwin-x86_64' in tarball_url:
+            if version_parse(version).public not in result:
+                result[version_parse(version).public] = tarball_url
+
+    return result
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download CMake binaries.')
-    parser.add_argument('version', type=str, nargs='*', help='version to download')
+    parser.add_argument('--latest_release', action='store_true',
+                        help='only download the latest release (default: False)')
+    parser.add_argument('--latest_patch', action='store_true',
+                        help='only download the latest patch version for each release (default: False)')
+    parser.add_argument('--release_candidates', action='store_true',
+                        help='also consider release candidates (default: False)')
     parser.add_argument('--tools_directory', metavar='DIR', default='tools',
                         help='path to the CMake binaries (default: "tools")')
     args = parser.parse_args()
 
-    tarball_urls = get_tarball_urls()
+    version_dict = create_version_dict()
+    versions = sorted([version_parse(version) for version in version_dict.keys()])
 
-    print('Downloading CMake releases...')
-    for tarball_url in tarball_urls:
-        if 'Darwin64' in tarball_url or 'Darwin-x86_64' in tarball_url:
-            print(tarball_url)
-            download_file(tarball_url)
+    if not args.release_candidates:
+        versions = [version for version in versions if not version.is_prerelease]
+
+    if args.latest_patch:
+        result = []
+        for major, minor in set([(version.major, version.minor) for version in versions]):
+            result.append([version for version in versions if version.major == major and version.minor == minor][-1])
+        versions = sorted(result)
+
+    if args.latest_release:
+        versions = versions[-1:]
+
+    for version in versions:
+        print('Downloading CMake {version}...'.format(version=version.public))
+        download_file(version_dict[version.public])
