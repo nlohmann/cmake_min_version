991751d39c5aa9bc9b277cee562aff8062175e15
Niels Lohmann <mail@nlohmann.me>
2024-04-02 20:58:33 +0200

    :art: run black and isort

--- cmake_downloader.py
+++ cmake_downloader.py
@@ -7,7 +7,7 @@ import re
 import tarfile
 import tempfile
 import zipfile
-from typing import List, Dict
+from typing import Dict, List
 
 import requests
 from packaging.version import parse as version_parse
@@ -15,22 +15,22 @@ from tqdm import tqdm
 
 
 def get_folders() -> List[str]:
-    url = 'https://cmake.org/files/'
+    url = "https://cmake.org/files/"
     html = requests.get(url).text
-    return list(re.findall(r'>v([0-9.]+)', html))
+    return list(re.findall(r">v([0-9.]+)", html))
 
 
 def get_tarball_urls_version(base_version: str) -> List[str]:
-    url = f'https://cmake.org/files/v{base_version}/'
+    url = f"https://cmake.org/files/v{base_version}/"
     html = requests.get(url).text
-    return sorted([url + filename for filename in re.findall(r'>(cmake-[0-9rc.]+-[^.]+(?:\.tar\.gz|\.zip))', html)])
+    return sorted([url + filename for filename in re.findall(r">(cmake-[0-9rc.]+-[^.]+(?:\.tar\.gz|\.zip))", html)])
 
 
 def get_tarball_urls() -> List[str]:
     folders = get_folders()
     result = []  # type: List[str]
 
-    print('Retrieving URLs...')
+    print("Retrieving URLs...")
     for folder in tqdm(folders):
         urls = get_tarball_urls_version(folder)
         result += urls
@@ -38,32 +38,32 @@ def get_tarball_urls() -> List[str]:
     return result
 
 
-def download_and_extract(url: str, path: str):
+def download_and_extract(url: str, path: str) -> None:
     # derive file directory name from URL
-    file_name_start_pos = url.rfind('/') + 1
+    file_name_start_pos = url.rfind("/") + 1
     file_name = url[file_name_start_pos:]
-    file_wo_ext = file_name.replace('.tar.gz', '').replace('.zip', '')
+    file_wo_ext = file_name.replace(".tar.gz", "").replace(".zip", "")
 
     if not os.path.exists(os.path.join(path, file_wo_ext)):
         response = requests.get(url, stream=True)
         response.raise_for_status()
-        file_size = int(response.headers['Content-Length'])
+        file_size = int(response.headers["Content-Length"])
 
         tmpdir = tempfile.TemporaryDirectory()
         full_file_name = os.path.join(tmpdir.name, file_name)
 
-        progress = tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
-        with open(full_file_name, 'wb+') as f:
+        progress = tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+        with open(full_file_name, "wb+") as f:
             for data in response:
                 progress.update(len(data))
                 f.write(data)
         progress.close()
 
-        if url.endswith('.zip'):
-            with zipfile.ZipFile(full_file_name, mode='r') as zip_ref:
+        if url.endswith(".zip"):
+            with zipfile.ZipFile(full_file_name, mode="r") as zip_ref:
                 zip_ref.extractall(path)
         else:
-            with tarfile.open(full_file_name, mode='r:gz') as tar:
+            with tarfile.open(full_file_name, mode="r:gz") as tar:
                 tar.extractall(path=path)
 
 
@@ -72,42 +72,76 @@ def create_version_dict(os: str) -> Dict[str, str]:
     result = dict()
 
     for tarball_url in tarball_urls:
-        version = re.findall(r'cmake-(([0-9.]+)(-rc[0-9]+)?)', tarball_url)[0][0]
+        version = re.findall(r"cmake-(([0-9.]+)(-rc[0-9]+)?)", tarball_url)[0][0]
 
-        if (os == 'macos' and ('Darwin64' in tarball_url or 'Darwin-x86_64' in tarball_url or 'macos-universal' in tarball_url)) \
-                or (os == 'linux' and ('Linux-x86_64' in tarball_url or 'linux-x86_64' in tarball_url)) \
-                or (os == 'windows' and ('win32-x86' in tarball_url or 'win64-x64' in tarball_url or 'windows-x86_64' in tarball_url)):
-            if version_parse(version).public not in result or \
-                    (version_parse(version).public in result and ('win64-x64' in tarball_url or 'windows-x86_64' in tarball_url)):
+        if (
+            (
+                os == "macos"
+                and ("Darwin64" in tarball_url or "Darwin-x86_64" in tarball_url or "macos-universal" in tarball_url)
+            )
+            or (os == "linux" and ("Linux-x86_64" in tarball_url or "linux-x86_64" in tarball_url))
+            or (
+                os == "windows"
+                and ("win32-x86" in tarball_url or "win64-x64" in tarball_url or "windows-x86_64" in tarball_url)
+            )
+        ):
+            if version_parse(version).public not in result or (
+                version_parse(version).public in result
+                and ("win64-x64" in tarball_url or "windows-x86_64" in tarball_url)
+            ):
                 result[version_parse(version).public] = tarball_url
 
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # get default value for current system
-    default_os = 'macos' if platform.system() == 'Darwin' else 'linux' if platform.system() == 'Linux' else 'windows' if platform.system() == 'Windows' else None
+    default_os = (
+        "macos"
+        if platform.system() == "Darwin"
+        else "linux" if platform.system() == "Linux" else "windows" if platform.system() == "Windows" else None
+    )
 
-    parser = argparse.ArgumentParser(description='Download CMake binaries.')
-    parser.add_argument('--os', help=f'OS to download CMake for (default: {default_os})',
-                        choices=['macos', 'linux', 'windows'], default=default_os)
-    parser.add_argument('--latest_release', action='store_true',
-                        help='only download the latest release (default: False)')
-    parser.add_argument('--latest_patch', action='store_true',
-                        help='only download the latest patch version for each release (default: False)')
-    parser.add_argument('--first_minor', action='store_true',
-                        help='only download the first minor version for each release (default: False)')
-    parser.add_argument('--release_candidates', action='store_true',
-                        help='also consider release candidates (default: False)')
-    parser.add_argument('--min_version', help='only download versions greater or equal than MIN_VERSION')
-    parser.add_argument('--max_version', help='only download versions less or equal than MAX_VERSION')
-    parser.add_argument('--tools_directory', metavar='DIR', default='tools',
-                        help='path to the CMake binaries (default: "tools")')
+    parser = argparse.ArgumentParser(description="Download CMake binaries.")
+    parser.add_argument(
+        "--os",
+        help=f"OS to download CMake for (default: {default_os})",
+        choices=["macos", "linux", "windows"],
+        default=default_os,
+    )
+    parser.add_argument(
+        "--latest_release",
+        action="store_true",
+        help="only download the latest release (default: False)",
+    )
+    parser.add_argument(
+        "--latest_patch",
+        action="store_true",
+        help="only download the latest patch version for each release (default: False)",
+    )
+    parser.add_argument(
+        "--first_minor",
+        action="store_true",
+        help="only download the first minor version for each release (default: False)",
+    )
+    parser.add_argument(
+        "--release_candidates",
+        action="store_true",
+        help="also consider release candidates (default: False)",
+    )
+    parser.add_argument("--min_version", help="only download versions greater or equal than MIN_VERSION")
+    parser.add_argument("--max_version", help="only download versions less or equal than MAX_VERSION")
+    parser.add_argument(
+        "--tools_directory",
+        metavar="DIR",
+        default="tools",
+        help='path to the CMake binaries (default: "tools")',
+    )
     args = parser.parse_args()
 
     version_dict = create_version_dict(os=args.os)
     versions = sorted([version_parse(version) for version in version_dict.keys()])
-    print(f'Found {len(versions)} versions from {versions[0]} to {versions[-1]}.')
+    print(f"Found {len(versions)} versions from {versions[0]} to {versions[-1]}.")
 
     if args.min_version:
         versions = sorted([version for version in versions if version >= version_parse(args.min_version)])
@@ -134,5 +168,5 @@ if __name__ == '__main__':
         versions = versions[-1:]
 
     for idx, version in enumerate(versions):
-        print(f'Downloading CMake {version.public} ({idx+1}/{len(versions)})...')
+        print(f"Downloading CMake {version.public} ({idx+1}/{len(versions)})...")
         download_and_extract(url=version_dict[version.public], path=args.tools_directory)
--- cmake_min_version.py
+++ cmake_min_version.py
@@ -2,14 +2,14 @@
 
 import argparse
 import glob
-import platform
 import math
 import os.path
+import platform
 import re
 import subprocess
 import sys
 import tempfile
-from typing import List, Optional, NamedTuple
+from typing import List, NamedTuple, Optional
 
 from packaging.version import parse as version_parse
 from termcolor import colored
@@ -29,22 +29,22 @@ class ConfigureResult:
 
         # try to read proposed minimal version from stderr output
         try:
-            self.proposed_version = re.findall(r'CMake ([^ ]+) or higher is required.', stderr)[0]
+            self.proposed_version = re.findall(r"CMake ([^ ]+) or higher is required.", stderr)[0]
 
             # support ranges
-            if '..' in self.proposed_version:
-                self.proposed_version = self.proposed_version.split('..')[0]
+            if ".." in self.proposed_version:
+                self.proposed_version = self.proposed_version.split("..")[0]
             # make sure all versions are major.minor.patch
-            if self.proposed_version.count('.') == 1:
-                self.proposed_version += '.0'
+            if self.proposed_version.count(".") == 1:
+                self.proposed_version += ".0"
         except IndexError:
             pass
 
         try:
-            self.reason = re.findall(r'CMake Error at (.*):', stderr)[0]
+            self.reason = re.findall(r"CMake Error at (.*):", stderr)[0]
         except IndexError:
             try:
-                self.reason = re.findall(r'CMake Error: ([^\n]+)', stderr)[0]
+                self.reason = re.findall(r"CMake Error: ([^\n]+)", stderr)[0]
             except IndexError:
                 pass
 
@@ -52,35 +52,44 @@ class ConfigureResult:
 def get_cmake_binaries(tools_dir: str) -> List[CMakeBinary]:
     binaries = []  # type: List[CMakeBinary]
     if platform.system() == "Windows":
-        filenames = glob.glob(tools_dir + '/**/bin/cmake.exe', recursive=True)
+        filenames = glob.glob(tools_dir + "/**/bin/cmake.exe", recursive=True)
     else:
-        filenames = glob.glob(tools_dir + '/**/bin/cmake', recursive=True)
+        filenames = glob.glob(tools_dir + "/**/bin/cmake", recursive=True)
 
     for filename in filenames:
         try:
-            version = re.findall(r'cmake-([^-]+)-', filename)[0]
+            version = re.findall(r"cmake-([^-]+)-", filename)[0]
             binaries.append(CMakeBinary(version, os.path.abspath(filename)))
         except IndexError:
             pass
 
-    print(f'Found {len(binaries)} CMake binaries from directory {tools_dir}\n')
+    print(f"Found {len(binaries)} CMake binaries from directory {tools_dir}\n")
     return sorted(binaries, key=lambda x: version_parse(x.version))
 
 
 def try_configure(binary: str, cmake_parameters: List[str]) -> ConfigureResult:
     tmpdir = tempfile.TemporaryDirectory()
-    proc = subprocess.Popen([binary] + cmake_parameters + ['-Wno-dev'],
-                            stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=tmpdir.name)
+    proc = subprocess.Popen(
+        [binary] + cmake_parameters + ["-Wno-dev"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE,
+        cwd=tmpdir.name,
+    )
     proc.wait()
 
-    return ConfigureResult(return_code=proc.returncode, stderr=proc.stderr.read().decode('utf-8'))
+    return ConfigureResult(return_code=proc.returncode, stderr=proc.stderr.read().decode("utf-8"))
 
 
 def binary_search(cmake_parameters: List[str], tools_dir: str, error_output: bool) -> Optional[CMakeBinary]:
     versions = get_cmake_binaries(tools_dir)  # type: List[CMakeBinary]
     cmake_versions = [len(cmake.version) for cmake in versions]
     if len(cmake_versions) == 0:
-        print(colored('Error: No CMake versions found in the tool dir. Make sure to run the cmake_downloader script first.', 'red'))
+        print(
+            colored(
+                "Error: No CMake versions found in the tool dir. Make sure to run the cmake_downloader script first.",
+                "red",
+            ),
+        )
         sys.exit(1)
     longest_version_string = max(cmake_versions) + 1  # type: int
 
@@ -98,24 +107,29 @@ def binary_search(cmake_parameters: List[str], tools_dir: str, error_output: boo
         remaining_versions = upper_idx - lower_idx + 1  # type: int
         remaining_steps = int(math.ceil(math.log2(remaining_versions)))  # type: int
 
-        print('[{progress:3.0f}%] CMake {cmake_version:{longest_version_string}}'.format(
-            progress=100.0 * float(steps - 1) / (steps + remaining_steps),
-            cmake_version=cmake_binary.version, longest_version_string=longest_version_string), end='', flush=True
+        print(
+            "[{progress:3.0f}%] CMake {cmake_version:{longest_version_string}}".format(
+                progress=100.0 * float(steps - 1) / (steps + remaining_steps),
+                cmake_version=cmake_binary.version,
+                longest_version_string=longest_version_string,
+            ),
+            end="",
+            flush=True,
         )
 
         result = try_configure(cmake_binary.binary, cmake_parameters)  # type: ConfigureResult
 
         if result.success:
-            print(colored('✔ works', 'green'))
+            print(colored("✔ works", "green"))
             last_success_idx = mid_idx
             upper_idx = mid_idx - 1
         else:
-            print(colored('✘ error', 'red'))
+            print(colored("✘ error", "red"))
             if error_output:
                 for line in result.stderr.splitlines():
-                    print(colored(f"       {line}", 'yellow'))
+                    print(colored(f"       {line}", "yellow"))
             elif result.reason:
-                print(colored(f'       {result.reason}', 'yellow'))
+                print(colored(f"       {result.reason}", "yellow"))
             proposed_binary = [x for x in versions if x.version == result.proposed_version]
             lower_idx = versions.index(proposed_binary[0]) if len(proposed_binary) else mid_idx + 1
 
@@ -137,37 +151,54 @@ def full_search(cmake_parameters: List[str], tools_dir: str, error_output: bool)
         remaining_versions = upper_idx - lower_idx + 1  # type: int
         remaining_steps = int(math.ceil(math.log2(remaining_versions)))  # type: int
 
-        print('[{progress:3.0f}%] CMake {cmake_version:{longest_version_string}}'.format(
-            progress=100.0 * float(steps - 1) / (steps + remaining_steps),
-            cmake_version=cmake_binary.version, longest_version_string=longest_version_string), end='', flush=True
+        print(
+            "[{progress:3.0f}%] CMake {cmake_version:{longest_version_string}}".format(
+                progress=100.0 * float(steps - 1) / (steps + remaining_steps),
+                cmake_version=cmake_binary.version,
+                longest_version_string=longest_version_string,
+            ),
+            end="",
+            flush=True,
         )
 
         result = try_configure(cmake_binary.binary, cmake_parameters)  # type: ConfigureResult
 
         if result.success:
-            print(colored('✔ works', 'green'))
+            print(colored("✔ works", "green"))
             if not last_success_idx or last_success_idx > steps - 1:
                 last_success_idx = steps - 1
         else:
-            print(colored('✘ error', 'red'))
+            print(colored("✘ error", "red"))
             if error_output:
                 for line in result.stderr.splitlines():
-                    print(colored(f"       {line}", 'yellow'))
+                    print(colored(f"       {line}", "yellow"))
             elif result.reason:
-                print(colored(f'       {result.reason}', 'yellow'))
+                print(colored(f"       {result.reason}", "yellow"))
 
     return versions[last_success_idx] if last_success_idx is not None else None
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Find the minimal required CMake version for a project.')
-    parser.add_argument('params', type=str, nargs='+', help='parameters to pass to CMake')
-    parser.add_argument('--tools_directory', metavar='DIR', default='tools',
-                        help='path to the CMake binaries (default: "tools")')
-    parser.add_argument('--full_search', default=False, action="store_true",
-                        help='Searches using a top down approach instead of a binary search (default: False)')
-    parser.add_argument("--error_details", default=False, action="store_true",
-                        help="Print the full stderr output in case of an error (default: False)")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find the minimal required CMake version for a project.")
+    parser.add_argument("params", type=str, nargs="+", help="parameters to pass to CMake")
+    parser.add_argument(
+        "--tools_directory",
+        metavar="DIR",
+        default="tools",
+        help='path to the CMake binaries (default: "tools")',
+    )
+    parser.add_argument(
+        "--full_search",
+        default=False,
+        action="store_true",
+        help="Searches using a top down approach instead of a binary search (default: False)",
+    )
+    parser.add_argument(
+        "--error_details",
+        default=False,
+        action="store_true",
+        help="Print the full stderr output in case of an error (default: False)",
+    )
     args = parser.parse_args()
 
     if args.full_search:
@@ -176,10 +207,14 @@ if __name__ == '__main__':
         working_version = binary_search(args.params, args.tools_directory, error_output=args.error_details)
 
     if working_version:
-        print('[100%] Minimal working version: {cmake} {version}'.format(
-            cmake=colored('CMake', 'blue'), version=colored(working_version.version, 'blue')))
+        print(
+            "[100%] Minimal working version: {cmake} {version}".format(
+                cmake=colored("CMake", "blue"),
+                version=colored(working_version.version, "blue"),
+            ),
+        )
 
-        print(f'\ncmake_minimum_required(VERSION {working_version.version})')
+        print(f"\ncmake_minimum_required(VERSION {working_version.version})")
 
     else:
-        print('[100%] {message}'.format(message=colored('ERROR: Could not find working version.', 'red')))
+        print("[100%] {message}".format(message=colored("ERROR: Could not find working version.", "red")))
